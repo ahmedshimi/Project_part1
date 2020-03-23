14a423ee47a79685151f65e247122a50d9b3d0e9
matt-sd-watson <matt.sd.watson@gmail.com>
2020-03-23 14:02:58 -0400

    rewrite insert method to avoid nosuchmethod error

--- project_part1/src/drawit/RoundedPolygon.java
+++ project_part1/src/drawit/RoundedPolygon.java
@@ -39,7 +39,8 @@ public class RoundedPolygon {
 	 * @param index
 	 * @param point
 	 */
-	public void insert​(int index,IntPoint point) {
+	
+	public void insert(int index, IntPoint point) {
 		setVertices(PointArrays.insert​(getVertices(), index, point));	
 	}
 	
