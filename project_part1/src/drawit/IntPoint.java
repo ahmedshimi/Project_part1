aff3fd815357a92e6efcb439ac92666930b4cc00
matt-sd-watson <matt.sd.watson@gmail.com>
2020-03-30 10:56:19 -0400

    precondition passes, don't fully understand why

--- project_part1/src/drawit/IntPoint.java
+++ project_part1/src/drawit/IntPoint.java
@@ -114,13 +114,14 @@ public final class IntPoint extends Object {
 		}
 	
 	
+
 	/**
 	 * 
 	 * @pre The line segments have at most one point in common. Assumes only straight lines exist between open line segments. 
 	 * 		If the lines are not collinear, then the precondition always holds
-	 * 
-	 * 		|(a.minus(b).isCollinearWith(c.minus(d)) && ((Math.max(a.getX(), b.getX()) <= Math.min(c.getX(), d.getX())) || Math.max(c.getX(), d.getX()) <= Math.min(a.getX(), b.getX()))) 
-	 * 			|| !(a.minus(b).isCollinearWith(c.minus(d))) 
+	 * 		|(!(b.minus(a).isCollinearWith(d.minus(c))) && !((Math.max(a.getX(), b.getX()) <= Math.min(c.getX(), d.getX()))) ||
+	 * 		|(!(b.minus(a).isCollinearWith(d.minus(c))) && !((Math.max(c.getX(), d.getX()) <= Math.min(a.getX(), b.getX()))))) ||
+	 * 		| (b.minus(a).isCollinearWith(d.minus(c)))
 	 * 
 	 * @return true if the open line segment ab intersects the open line segment cd.
 	 */
